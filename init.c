26cc53f533b44b11cb306df2f5055ba13fe0c60a
Johannes Thumshirn <jthumshirn@suse.de>
2016-11-22 13:11:52 +0100

    init: add template for do_test function
    Add template for do_test function and it's environment.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- init.c
+++ init.c
@@ -5,8 +5,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+enum test_result { TEST_PASS, TEST_FAIL };
+
+enum test_result do_test(void)
+{
+	return TEST_PASS;
+}
+
 int main(void)
 {
+	enum test_result ret;
 	int err;
 
 	err = mount("none", "/proc", "proc", 0, NULL);
@@ -19,10 +27,15 @@ int main(void)
 	if (err)
 		goto panic;
 
+	ret = do_test();
+	if (ret == TEST_PASS)
+		printf("PASS\n");
+	else
+		printf("FAIL\n");
+
 	umount("/dev");
 	umount("/sys");
 	umount("/proc");
-
 
 	reboot(RB_POWER_OFF);
 
